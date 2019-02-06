169175218a504485cafc33b26c44b3247b332a58
Arkadiro <wocsom@inbox.ru>
2019-02-07 00:50:58 +0300

    router + from validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { Routes } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(Routes),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,5 @@
+import { RegisterComponent } from './register/register.component';
+
+export const Routes = [
+  { path: 'auth/register', component: RegisterComponent }
+];
